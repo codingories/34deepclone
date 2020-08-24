0e998b6e136a879c1eacfbf4283badf43d90b7b5
朱骏杰 <ories.zhu@mochain.vip>
2020-08-24 23:27:26 +0800

    搞定函数的深拷贝

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 function deepClone(source){
   if (source instanceof Object){
-    const dist = new Object();
-    for(let key in source){
-      dist[key] = deepClone(source[key]);
+    // const a = { name: "方方", child: {name: "小方方"}}
+    if(source instanceof Array){
+      const dist = new Array();
+      for(let key in source){
+        dist[key] = deepClone(source[key]);
+      }
+      return dist;
+    } else if(source instanceof Function){
+      const dist = function() {
+        return source.apply(this, arguments)
+      }
+      for(let key in source){
+        dist[key] = deepClone(source[key]);
+      }
+      return dist;
+    } else{
+      const dist = new Object();
+      for(let key in source){
+        dist[key] = deepClone(source[key]);
+      }
+      return dist;
     }
-    return dist;
   }
-
   return source
 }
 
--- test/index.js
+++ test/index.js
@@ -38,7 +38,27 @@ describe("deepClone", ()=>{
       assert(a.name === a2.name)
       assert(a.child !== a2.child)
       assert(a.child.name === a2.child.name)
-
+    });
+    it("能够复制数组对象", ()=>{
+      const a = [[11,12],[21,22],[31,32]];
+      const a2 = deepClone(a);
+      assert(a !== a2);
+      assert(a[0] !== a2[0]);
+      assert(a[1] !== a2[1]);
+      assert(a[2] !== a2[2]);
+      assert.deepEqual(a,a2);
+    });
+    it("能够复制数组对象", ()=>{
+      const a = function(x, y){
+        return x+y;
+      }
+      a.xxx = { yyy : { zzz:1 } };
+      const a2 = deepClone(a);
+      assert(a !== a2);
+      assert(a.xxx.yyy.zzz === a2.xxx.yyy.zzz);
+      assert(a.xxx !== a2.xxx);
+      assert(a(1,2) === a2(1,2));
     })
+
   })
 })
